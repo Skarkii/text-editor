fcb73c23e081c3a67517a51f91d88697e2864b52
Skarkii <lucas@kuja-halkola.com>
2023-10-23 04:44:16 +0200

    commit #0002, We can type
    Typing has been added, this includes pressing moving with arrow keys,
    inserting text, removing text, pressing enter and back and everything
    will work as expected within a text editor. File save/load functions
    has been started, save works to the extent of 2 bytes ascii
    characters, characters such as 'ö' requires 4 and is causing
    issues. Load needs to be able to handle newlines and generating a map
    containing the row lengths.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,51 @@
 #include <ncurses.h>
 #include <string>
+#include <map>
+#include <fstream>
+#include <assert.h>
+#include <sstream>
+std::map<unsigned int, unsigned int> eol;
+unsigned int col_margin = 3;
+
+bool save_contents(std::string filename) {
+  std::ofstream file;
+  file.open(filename);
+
+  if(!file.is_open()) {
+    assert(false && "could not open file");
+  }
+  char str[getmaxx(stdscr)];
+  std::stringstream buffer;
+  int i = 0;
+  while(eol.count(i) != 0) {
+    mvinnstr(i, col_margin, str, eol[i] - col_margin);
+    buffer << str << '\n';
+    i ++;
+  }
+
+  file << buffer.str();
+  return true;
+}
+
+bool load_contents(std::string filename) {
+  std::fstream file;
+
+  file.open(filename);
+
+  if(!file.is_open()) {
+    assert(false && "could not open file");
+  }
+
+  std::stringstream buffer;
+  char c;
+  while(file >> c) {
+    buffer << c;
+  }
+
+  mvprintw(0, col_margin, "%s", buffer.str().c_str());
+  
+  return true;
+}
 
 int main(int argc, const char** argv) {
 
@@ -10,47 +56,103 @@ int main(int argc, const char** argv) {
   initscr(); // Creates stdscr
   raw(); // Disables line-buffering
   keypad(stdscr, TRUE); // Enables F1-F12 etc
+
   
   noecho();
-
   unsigned int row, col;
   row = 0;
-  col = 0;
+  col = col_margin;
+  eol[0] = col;
   
   move(row, col);
   refresh();
   int temp = -1;
   while(ch != KEY_F(1)) {
-
+  
     switch(ch) {
+    case KEY_F(9):
+      save_contents("savefile");
+      break;
+    case KEY_F(10):
+      load_contents("savefile");
+      break;
     case KEY_LEFT:
-      if(col < 1) { break; }
+      if(col <= col_margin) { break; }
       col --;
       break;
     case KEY_RIGHT:
-      col++;
+      if(col + 1 <= eol[row]) {
+	col++;
+      }
       break;
     case KEY_UP:
       if(row < 1) { break; }
       row --;
+      if(col > eol[row]) { col = eol[row]; }
       break;
     case KEY_DOWN:
-      row++;
+      if(eol.count(row + 1) > 0) {
+          row++;	
+      }
+      if(col > eol[row]) { col = eol[row];}
       break;
     case '\n':
-      row++;
-      col = 0;
+      {
+	char str[getmaxx(stdscr) - col];
+	int len = eol[row] - col + col_margin;
+	eol[row] = col;
+	instr(str);
+	printw("\n");
+	row = row + 1;
+	col = col_margin;
+	move(row, col);
+        
+	insertln();
+	printw("%s", str);
+
+	for(unsigned int i = eol.rbegin()->first + 1; i > row; i --) {
+	  eol[i] = eol[i - 1];
+	}
+	
+	eol[row] = len;
+      }
       break;
     case KEY_BACKSPACE:
-      if(col < 1) {
-	if(row > 0) {
-	  row --;
+      {
+	int len = eol[row] - col;
+	char str[getmaxx(stdscr) - col];
+	instr(str);
+	if(col <= col_margin) {
+	  if(row > 0) {
+	    row = row - 1;
+	    col = eol[row];
+	    move(row, col);
+	    printw("%s", str);
+	    eol[row] = eol[row] + len;
+	    deleteln();
+	    unsigned int i = row + 1;
+	    while(eol.count(i) > 0) {
+	      if(eol.count(i + 1) <= 0) {
+		eol.erase(i);
+		break;
+	      } else {
+	      eol[i] = eol[i + 1];
+	      i++;
+	      }
+	    }
+	    break;
+	  }
+	  break;
 	}
-	break;
+	col --;
+	move(row, col);
+	printw("%s ", str);
       }
-      col --;
-      move(row, col);
-      printw(" ");
+      eol[row] = eol[row] - 1;
+      break;
+    case 9:
+      break;
+    case 353:
       break;
     case -1:
       break;
@@ -59,6 +161,14 @@ int main(int argc, const char** argv) {
 	if(ch == 130) {
 	  break;
 	}
+	{
+	  int char_count = getmaxx(stdscr) - col;
+	  char str[char_count];
+	  instr(str);
+	  mvprintw(row, col+1,"%s", str);
+	  move(row, col);
+	}
+	
 	switch(ch) {
 	  break;
 	case 163:
@@ -79,7 +189,6 @@ int main(int argc, const char** argv) {
 	case 189:
 	  printw("½");
 	case 190:
-	  printw("½");
 	  break;
 	case 165:
 	  printw("å");
@@ -104,6 +213,7 @@ int main(int argc, const char** argv) {
 	  printw("%c", ch);
 	  break;
 	}
+	eol[row] = eol[row] + 1;
 	col++;
       }
       
@@ -117,11 +227,15 @@ int main(int argc, const char** argv) {
     std::string key_pressed;
     key_pressed = "key pressed: " + std::to_string(ch) + "     ";
     std::string key_at_cursor;
-    key_at_cursor = "key at cursor: " + std::to_string(mvinch(row, col));
-    mvprintw(8, 5, "%s", key_at_cursor.c_str());
-    mvprintw(7, 5, "%s", key_pressed.c_str());
-    mvprintw(5, 5, "%s", current_row.c_str());
-    mvprintw(6, 5, "%s", current_col.c_str());
+    key_at_cursor = "key at cursor: " + std::to_string(mvinch(row, col)) + "         ";
+    std::string row1len;
+    row1len += "row 1 len: " + std::to_string(eol[0]) + "    ";
+    //mvprintw(35, 0, "%s", row2len.c_str());
+    //mvprintw(34, 0, "%s", row1len.c_str());
+    //mvprintw(30, 0, "%s", key_at_cursor.c_str());
+    //mvprintw(31, 0, "%s", key_pressed.c_str());
+    //mvprintw(32, 0, "%s", current_row.c_str());
+    //mvprintw(33, 0, "%s", current_col.c_str());
     move(row, col);
     refresh();
     last_ch = ch;
